2c1d52533d3c662e0de604be9f8929882bc9bffc
uduak <lorduduak@gmail.com>
2022-09-10 18:02:26 +0100

    added aos animations

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+// preloader
 const preloader = document.querySelector('.preloader');
 const main = document.querySelector('.main');
 
 window.addEventListener('load', ()=>{
     window.setTimeout(()=>{
         preloader.style.display = 'none';
-        main.style.display = 'block';
+        main.style.visibility = 'visible';
+        document.body.style.overflowY = 'auto';
     }, 2000);
+
+    window.setTimeout(()=>{
+        typeWriter();
+    }, 3500);
 })
+
+// text typing effect
+let i = 0;
+let txt = 'a web developer'; /* The text */
+let speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("hero-txt").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
