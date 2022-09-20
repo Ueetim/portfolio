010924bba8a6ff1c8c4d3702b8a691113749ada1
uduak <lorduduak@gmail.com>
2022-09-20 17:25:08 +0100

    highlight current section on scroll

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-// preloader
-const preloader = document.querySelector('.preloader');
-const main = document.querySelector('.main');
+// // preloader
+// const preloader = document.querySelector('.preloader');
+// const main = document.querySelector('.main');
 
 window.addEventListener('load', ()=>{
-    // window.setTimeout(()=>{
-    //     preloader.style.display = 'none';
-    //     main.style.visibility = 'visible';
-    //     document.body.style.overflowY = 'auto';
-    // }, 2000);
-
     window.setTimeout(()=>{
         typeWriter();
     }, 1600);
@@ -26,3 +20,47 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+// indicate current section nav btn
+// let navItems = document.querySelectorAll('.navItem');
+// let section = document.getElementsByTagName('section');
+// // console.log(section);
+
+// window.addEventListener('scroll', ()=>{
+//   let scrollHeight = window.pageYOffset;
+//   // console.log(scrollHeight)
+//   let a = [];
+//   for (let i = 0; i < section.length; i++) {
+//     navItems[i].classList.remove('active');
+//     if ((section[i].offsetTop - 300) < scrollHeight) {
+//       a.push(i);
+//       let item = navItems[a.length - 1];
+//       console.log(item.dataset.id);
+//       item.classList.add('active')
+//       if (navItems[i] !== item){
+//         navItems[i].classList.remove('active');
+//       }
+//     }
+//   }
+// })
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".navItem");
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 300) {
+      current = section.getAttribute("id"); 
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.dataset.id == current) {
+      li.classList.add("active");
+    }
+  });
+};
