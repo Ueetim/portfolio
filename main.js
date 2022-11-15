8499d38dde261022d341f8d9ce05a6f9e55ae81f
uduak <lorduduak@gmail.com>
2022-11-15 11:55:49 +0100

    project lists displayed from javascript array

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-// // preloader
-// const preloader = document.querySelector('.preloader');
-// const main = document.querySelector('.main');
+// projects list
+let projects = [
+  {
+    name: 'Food Fusion',
+    desc: 'A simple food ecommerce platform interface implemented with Angular and TypeScript',
+    lang: 'HTML, CSS, TS, Angular',
+    imgUrl: 'images/projects/foodfusion.png',
+    link: 'https://foodfusion.vercel.app'
+  },
+  {
+    name: 'Youtube Search',
+    desc: 'A YouTube Search Component which utilizes the YouTube Data Api to make searches and display matching videos',
+    lang: 'HTML, CSS, TS, Angular',
+    imgUrl: 'images/projects/youtube.png',
+    link: 'https://youtubeapi-tau.vercel.app'
+  },
+  {
+    name: 'Video Game DB',
+    desc: 'A video game database where users can search and view information about hundreds of thousands of games',
+    lang: 'HTML, SCSS, TS, Angular',
+    imgUrl: 'images/projects/vid.png',
+    link: 'https://gamedb-delta.vercel.app'
+  },
+  {
+    name: 'Basita Brimix',
+    desc: "BASITA is one of Nigeria's leading paint companies with a wealth of products and services ...",
+    lang: 'HTML, CSS, JS',
+    imgUrl: 'images/projects/basita.png',
+    link: 'https://basitabrimix.com'
+  },
+  {
+    name: 'Senpai Academy',
+    desc: "A school that makes a difference...",
+    lang: 'HTML, CSS, JS',
+    imgUrl: 'images/projects/senpai.png',
+    link: 'https://senpaiacademy.netlify.app'
+  },
+  {
+    name: 'Hotel Pablo',
+    desc: "The best place to stay!!!",
+    lang: 'HTML, CSS, JS',
+    imgUrl: 'images/projects/hotel.png',
+    link: 'https://hotelpablo.netlify.app'
+  }
+]
 
 window.addEventListener('load', ()=>{
     window.setTimeout(()=>{
@@ -20,29 +62,6 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-
-// indicate current section nav btn
-// let navItems = document.querySelectorAll('.navItem');
-// let section = document.getElementsByTagName('section');
-// // console.log(section);
-
-// window.addEventListener('scroll', ()=>{
-//   let scrollHeight = window.pageYOffset;
-//   // console.log(scrollHeight)
-//   let a = [];
-//   for (let i = 0; i < section.length; i++) {
-//     navItems[i].classList.remove('active');
-//     if ((section[i].offsetTop - 300) < scrollHeight) {
-//       a.push(i);
-//       let item = navItems[a.length - 1];
-//       console.log(item.dataset.id);
-//       item.classList.add('active')
-//       if (navItems[i] !== item){
-//         navItems[i].classList.remove('active');
-//       }
-//     }
-//   }
-// })
 
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".navItem");
@@ -64,3 +83,24 @@ window.onscroll = () => {
     }
   });
 };
+
+let projectsCon = document.querySelector('.projects');
+
+// PROJECTS DISPLAY
+projects.forEach((project) => {
+  projectsCon.innerHTML += `
+  <a href="${project.link}" target="_blank">
+    <div class="project" style="background-image: url(${project.imgUrl});">
+        <div class="project-info">
+            <ion-icon name="folder-outline"></ion-icon>
+            <div class="project-details dflex f-column">
+                <div>
+                    <p class="project-name"><strong>${project.name}</strong></p>
+                    <p><small>${project.desc}</p>
+                </div>
+                <p><small>${project.lang}</small></p>
+            </div>
+        </div>
+    </div>
+  </a>` 
+})
